6de6e56962e5f36f7180ac569f127826abef5ec2
Uri Shaked <uri@urishaked.com>
2024-02-01 23:16:53 +0200

    feat: generate version.json and include it in releases
    will be used in the future for update notification mechanism

--- tools/build.js
+++ tools/build.js
@@ -1,9 +1,28 @@
 import { execSync } from 'child_process';
 import { build } from 'esbuild';
-import { readFileSync } from 'fs';
+import { mkdirSync, readFileSync, writeFileSync } from 'fs';
 
 const { version } = JSON.parse(readFileSync('package.json', 'utf8'));
 const sha = execSync('git rev-parse --short=12 HEAD').toString().trim();
+
+const installCommands = {
+  win32: 'iwr https://wokwi.com/ci/install.ps1 -useb | iex',
+  default: 'curl -L https://wokwi.com/ci/install.sh | sh',
+};
+
+mkdirSync('dist/bin', { recursive: true });
+writeFileSync(
+  'dist/bin/version.json',
+  JSON.stringify(
+    {
+      version,
+      sha,
+      install: installCommands,
+    },
+    null,
+    2,
+  ),
+);
 
 const options = {
   platform: 'node',
